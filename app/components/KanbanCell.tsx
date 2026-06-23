28d17944cf99ba22db5e2600d518ba4d7e2606dc
Wladimir <wladimirss142@gmail.com>
2026-06-23 00:56:40 -0400

    adjust: scrollbar animation

--- app/components/KanbanCell.tsx
+++ app/components/KanbanCell.tsx
@@ -20,7 +20,7 @@ export default function KanbanCell({ sprintId, statusId, activities }: KanbanCel
 	return (
 		<div
 			ref={setNodeRef}
-			className={`w-[280px] shrink-0 h-full min-h-0 rounded-xl border p-2 flex flex-col gap-3 transition-all duration-300 overflow-y-auto scrollbar-hide
+			className={`w-[280px] shrink-0 h-full min-h-0 rounded-xl border p-2 flex flex-col gap-3 transition-all duration-300 overflow-y-auto
         ${isOver ? 'border-solid border-(--color-muted) bg-black/4 dark:bg-white/5 shadow-inner' : 'border-dashed border-(--color-border) bg-black/2 dark:bg-white/2'}
       `}
 		>
